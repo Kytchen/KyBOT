dfdc364ce9919198d15c635c32656ce15b69b76a
Riky Ripaldo <157950936+Kytchen@users.noreply.github.com>
2024-01-28 19:55:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-const { 
-  default: makeWASocket, 
-  useMultiFileAuthState 
-} = require('@whiskeysockets/baileys')
+const {
+  default: makeWASocket, useMultiFileAuthState
+} = require("@whiskeysockets/baileys");
+const pino = require("pino");
+const readline = require("readline");
 
-const RikyBOT = process.argv.includes('--RikyBOT')
+const baca = readline.createInterface({input: process.stdin, output: process.stdout});
+const rikybot = process.argv.includes("--RikyBOT");
 
-const KyBOT = async() => {
-  const riky = async makeWASocket({
-   printQRCodeTerminal: !RikyBOT 
-  })
+async function connectToWhatsApp() {
+  const { state, saveCreds } = await useMultiFileAuthState("riky");
+  const riky = makeWASocket({
+    printQRInTerminal: !rikybot,
+    auth: state,
+    broswer: rikybot ? ["RikyBOT", "Firefox", "1.0.0"] : ["Riky Ripaldo", "Safari", "1.0.0"],
+    logger: pino({ level: "silent" })
+  });
+
+  if (rikybot && !riky.authState.creds.registered) {
+    const pertanyaan = (tanya) => new Promise(async (resolve) => {
+      baca.question(tanya, (jawaban) => {
+        resolve(jawaban);
+        baca.close();
+      });
+    });
+    const nomor = await pertanyaan("Masukkan nomor WhatsApp : ");
+    setTimeout(async function() {
+      const codes = await riky.requestPairingCode(nomor);
+      console.log(`Pairing Kode Anda : ${codes}`);
+    }, 3000);
+  }
+
+  riky.ev.on("creds.update", saveCreds);
+  riky.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      console.log("Koneksi terputus, mencoba menghubungkan kembali...");
+      await connectToWhatsApp();
+    } else if (connection === "open") {
+      console.log("Koneksi terhubung ke nomor " + riky.user.id.split(":")[0]);
+    }
+  });
+
+  riky.ev.on("messages.upsert", async ({pesan}) => {
+    const chat = pesan.messages[0];
+    console.log(chat);
+    function reply(text) {
+      riky.sendMessage(chat.key.remoteJid, {text: text}, {quoted: chat});
+    }
+    if (chat.message.conversation.toLowercase() === "ping") {
+      reply("pong");
+    }
+  });
 }
 
-KyBOT()
+connectToWhatsApp();
